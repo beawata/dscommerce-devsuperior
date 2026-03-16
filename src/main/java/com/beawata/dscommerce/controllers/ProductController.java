e84e11ad2f08b1530b55430d76c3936299fbe627
beawata <beawata.dev@gmail.com>
2026-03-16 16:35:27 -0300

    GET Products paginado

--- src/main/java/com/beawata/dscommerce/controllers/ProductController.java
+++ src/main/java/com/beawata/dscommerce/controllers/ProductController.java
@@ -1,15 +1,16 @@
 package com.beawata.dscommerce.controllers;
 
 import com.beawata.dscommerce.dto.ProductDTO;
-import com.beawata.dscommerce.entities.Product;
-import com.beawata.dscommerce.repositories.ProductRepository;
 import com.beawata.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,5 +23,10 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id){
         return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
     }
 }
