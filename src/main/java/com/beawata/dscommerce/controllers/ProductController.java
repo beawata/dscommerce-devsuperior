a81532c4bdd0e3dbe11cf1d3d73b54b709567c54
beawata <beawata.dev@gmail.com>
2026-03-16 15:10:15 -0300

    criando DTO e estrutura de camadas

--- src/main/java/com/beawata/dscommerce/controllers/ProductController.java
+++ src/main/java/com/beawata/dscommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.beawata.dscommerce.controllers;
 
+import com.beawata.dscommerce.dto.ProductDTO;
+import com.beawata.dscommerce.entities.Product;
+import com.beawata.dscommerce.repositories.ProductRepository;
+import com.beawata.dscommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
-    @GetMapping
-    public String teste(){
-        return "Ola mundo!";
+    @Autowired
+    private ProductService service;
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id){
+        return service.findById(id);
     }
 }
